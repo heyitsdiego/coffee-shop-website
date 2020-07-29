6003d9153d99cb9418d9e9177ae8dfc2616e66a0
Diego <dcoaguila@gmail.com>
2020-07-29 16:32:45 -0700

    Initial commit

--- footer.js
+++
@@ -1,8 +0,0 @@
-const footer = () => {
-  const footerContainer = document.createElement('footer');
-  const footerParagraph = document.createElement('p');
-  footerParagraph.innerText = '3210 Saratoga Blvd • Saratoga, CA 90001';
-  footerContainer.appendChild(footerParagraph);
-  return footerContainer;
-};
-export default footer;
